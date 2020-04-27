a58e3813bd8ad3e12801dbdc0b798f18cdea924c
Grzegorz Krug <grzesiekk321@gmail.com>
2020-04-28 00:29:20 +0200

    save

--- src/command-center/src/App.js
+++ src/command-center/src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import logo from './banana.png';
 import './App.css';
+
+const Child = ({ match }) => (
+  <div>
+    <h3>You are here @{match.params.id}</h3>
+  </div>
+)
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome traveler</h2>
+          </div>
+          <div className="Content-Table">
+            <ul>
+              <li><Link to='/netlifx'>Netflix</Link></li>
+              <li><Link to='/discord'>Discord</Link></li>
+              <li><Link to='/gitlab'>Gitlab</Link></li>
+              <li><Link to='/yahoo'>Yahoo</Link></li>
+            </ul>
+          </div>
+          <Route path="/:id" component={Child} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
