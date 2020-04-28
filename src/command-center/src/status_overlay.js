46f999992b1297541aa9b5a3a17004c1f897bbd1
Grzegorz Krug <grzesiekk321@gmail.com>
2020-04-28 17:00:17 +0200

    overlay

---
+++ src/command-center/src/status_overlay.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Status_overlay extends Component {
+    render() {
+        return (
+            <h1>This will be overlay</h1>
+        )
+    }
+}
