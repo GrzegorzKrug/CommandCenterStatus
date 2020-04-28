97f5478ea197af2c8737b6341719110cfac043b6
Grzegorz Krug <grzesiekk321@gmail.com>
2020-04-28 16:09:19 +0200

    banana in card

--- src/command-center/src/my_container.js
+++ src/command-center/src/my_container.js
@@ -1,18 +1,22 @@
-import React, {Component} from 'react';
-import {Card, Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-export default class MyBox extends Component{
+export default class MyBox extends Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-        {/* <Card.Img variant='bottom' src="./banana.png" /> */}
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Img width="40%" src={require('./banana.png')}/>
+          
+          <Card.Body>
+            <h3>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </h3>
+          </Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
         </Card.Body>
       </Card>
     );
